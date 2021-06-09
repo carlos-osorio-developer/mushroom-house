0ea2a044bc0d97a43b4d14aa476dc0db0edcdbff
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-09 18:52:56 -0500

    Add Vote model, controller, migration and tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_234850) do
+ActiveRecord::Schema.define(version: 2021_06_09_235128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,4 +30,15 @@ ActiveRecord::Schema.define(version: 2021_06_09_234850) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_votes_on_article_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "votes", "articles"
+  add_foreign_key "votes", "users"
 end
