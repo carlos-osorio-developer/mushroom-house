520fdb9a1821b6d7ad7d2b269324c8eb1f92257f
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-10 14:28:45 -0500

    Add Sessions controller and helper methods
    Signed-off-by: carlos-osorio-developer <osorio.uis@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+def new
+end
+
+def create
+    user = User.find_by(name: params[:session][:name].downcase)
+    if user
+    log_in user
+    redirect_back_or user
+    else
+    flash.now[:danger] = 'Invalid email/password combination'
+    render 'new'
+    end
+end
+
+def destroy
+    log_out
+    redirect_to root_url
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,7 @@ class UsersController < ApplicationController
   # POST /users or /users.json
   def create
     @user = User.new(user_params)
+    @user.name = @user.name.downcase
 
     respond_to do |format|
       if @user.save
