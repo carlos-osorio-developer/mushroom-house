5dfdb8941f941ac7a813207ca9e987630830bb0f
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-09 20:26:04 -0500

    Add associations between votes, articles and users

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,6 +3,8 @@ class Article < ApplicationRecord
 
   has_many :listings, foreign_key: :article_id
   has_many :categories, through: :listings
+  
+  has_many :votes
 
   validates :title, presence: true, length: { in: 8..100 }
   validates :text, presence: true, length: { minimum: 30 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :articles, foreign_key: 'author_id', dependent: :destroy
+  has_many :votes
 
   validates :name, presence: true, length: { minimum: 3 }
 end
