06286f4b0d8c2efc8eb0dd2806de9d402cf26438
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-09 20:15:00 -0500

    Add associations between articles and categories through listings table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ApplicationRecord
   belongs_to :author, class_name: 'User'
 
+  has_many :listings, foreign_key: :article_id
+  has_many :categories, through: :listings
+
   validates :title, presence: true, length: { in: 8..100 }
   validates :text, presence: true, length: { minimum: 30 }
   validates :image, presence: true
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,6 @@
 class Category < ApplicationRecord
+  belongs_to :listed_category, class_name: 'Category'
+  
+  has_many :listings, foreign_key: :category_id
+  has_many :articles, through: :listings
 end
