61463f87105d44f8cd79ffed8982bcf3bb64928d
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-10 15:48:49 -0500

    Add header layout
    Signed-off-by: carlos-osorio-developer <osorio.uis@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module MushroomHouse
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.paths << Rails.root.join("app", "assets", "images", "levers")
 
     # Configuration for the application, engines, and railties goes here.
     #
