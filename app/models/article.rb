9fa816d99e9222b19c52b1037d310b7b5abc7c64
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-09 19:25:36 -0500

    Add validations for Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,7 @@
 class Article < ApplicationRecord
   belongs_to :author, class_name: 'User'
+
+  validates :title, presence: true, length: { in: 8..100 }
+  validates :text, presence: true, length: { minimum: 30 }
+  validates :image, presence: true
 end
