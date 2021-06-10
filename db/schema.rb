cf32dd8dd49678397763d46d78a84c1faadc8ebc
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-09 20:02:39 -0500

    Add model and migration for listings between articles and categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_235547) do
+ActiveRecord::Schema.define(version: 2021_06_10_005727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,15 @@ ActiveRecord::Schema.define(version: 2021_06_09_235547) do
     t.integer "priority"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.bigint "article_id"
+    t.bigint "category_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_listings_on_article_id"
+    t.index ["category_id"], name: "index_listings_on_category_id"
   end
 
   create_table "users", force: :cascade do |t|
