b1441396727448e375fd0e4d13c206c2fc4aa62a
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-09 20:26:42 -0500

    Add unique validation for Vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,12 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :article  
+
+  validate :vote_is_unique
+
+  def vote_is_unique
+    if Vote.where(user_id: user_id, article_id: article_id).exists?
+      self.errors.add(:user_id, 'has already voted for this article')
+    end
+  end
 end
