8e78a28eb01da5e04b742a6432fb721e232b869d
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-11 14:34:55 -0500

    Add routes, controllers and views to upvote an article
    Signed-off-by: carlos-osorio-developer <osorio.uis@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root "articles#index"
+  root "categories#index"
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  post '/votes/:article_id', to: 'votes#create', as: 'create_vote'
 end
