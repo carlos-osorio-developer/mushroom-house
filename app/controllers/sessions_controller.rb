39f9bf32b279a6d7770844215c7af492091af3aa
carlos-osorio-developer <osorio.uis@gmail.com>
2021-06-13 17:59:29 -0500

    Edit style for shared/flash view
    Signed-off-by: carlos-osorio-developer <osorio.uis@gmail.com>

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ def create
     user = User.find_by(name: params[:session][:name].downcase)
     if user
     log_in user
-    redirect_back_or user
+    redirect_back_or root_path
     else
     flash.now[:danger] = 'Invalid email/password combination'
     render 'new'
